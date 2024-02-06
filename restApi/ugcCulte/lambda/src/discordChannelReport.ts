b2a1d7547dd18ea97f8b70a3960a4a5c1687bc17
Nicolas Cordier <nicolas2.cordier@epitech.eu>
2024-02-06 23:30:39 +0100

    Report movie release date in last Discord message field

--- restApi/ugcCulte/lambda/src/discordChannelReport.ts
+++ restApi/ugcCulte/lambda/src/discordChannelReport.ts
@@ -123,11 +123,11 @@ export async function reportMovieToChannel(channelId: string, cinemaId: number, 
             description: movie.description || '-',
             thumbnail: thumbnail,
             fields: getOptionalFields([
-                { name: 'Date de sortie', value: movie.releaseDateFR, inline: true },
                 { name: 'Genres', value: movie.genders, inline: true },
                 { name: 'Réalisateurs', value: movie.directors, inline: true },
                 { name: 'Acteurs', value: movie.actors, inline: true },
                 { name: 'Durée', value: movie.durationFR, inline: true },
+                { name: 'Date de sortie', value: movie.releaseDateFR, inline: true },
                 ...movie.showings.map((s) => ({
                     name: `${s.lang} - ${s.startDateFR} (fin ${s.endDateFR})`,
                     value: `[Réserver](${s.bookingUrl})`,
